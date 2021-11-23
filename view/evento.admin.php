60c23840432fed84b469d3adee6e751f73d6ea75
raulgarcia69 <raulgarcia0144@gmail.com>
2021-11-23 19:16:44 +0100

    V1

--- view/evento.admin.php
+++ view/evento.admin.php
@@ -1,48 +1,39 @@
-<?php
+<!DOCTYPE html>
+<?php 
 include_once '../services/connection.php';
 ?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
     <title>Actividades de Barcelona</title>
+    <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 </head>
 <body>
-    <div class="menu">
-        <div class="logo"><a href="#"><img src="../img/logo.svg"></a></div>
-    </div>
-        
-    <div class="evento-todos">
-        <h1>Actividades destacadas de los próximos días </h1>
-
-    
+<div class="logout"><a href="../services/kill-login.php"><img src="../img/logout.png" ></a></div>
 <?php
+    function limit_words($string, $word_limit)
+    {
+        $words = explode(" ",$string);
+        return implode(" ", array_splice($words, 0, $word_limit));
+    }
     $evento=$pdo->prepare("SELECT * from tbl_evento");
     $evento->execute();
     $evento=$evento->fetchAll(PDO::FETCH_ASSOC);
     foreach ($evento as $evento) {
-    ?>
-    <div class="evento-individual">
-        <div class="evento-individual-contenido">
-            <h1><?php echo $evento['nombre_even']; ?></h1>
-            <p><?php echo $evento['descripcion_even']; ?></p>
-        </div>
-        <div class="inscribir"><button type='button' class='btn btn-light btn-lg' >Inscribirte</button></div>
-        
-        
+?>
+    <div class="evento">
+    <h3><?php echo $evento['nombre_even']; ?></h3>
+    <p><?php echo limit_words($evento['descripcion_even'],20); ?>...</p>
     </div>
-    
-    <?php
+
+<?php
         }
     ?>
-
-</div>
-                
+    
 </body>
 </html>
